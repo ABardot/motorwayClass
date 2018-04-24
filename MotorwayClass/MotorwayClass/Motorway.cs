f0d12f641a3c643d8fab3fbcb8265cb1c43bec8f
ABardot <aabardot5@gmail.com>
2018-04-24 18:59:11 -0400

    First Commit, added Motorway Class with Properties and Constructors.

---
+++ MotorwayClass/MotorwayClass/Motorway.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MotorwayClass
+{
+    class Motorway
+    {
+        // Properties
+        public string Name { get; set; }
+        public string MotorwayType { get; set; }
+        public char Direction { get; set; }
+        public string Surface { get; set; }
+        public int NumberOfLanes { get; set; }
+        public bool Toll { get; set; }
+        public string Maintainance { get; set; }
+
+        // Constructors
+        public Motorway()
+        {
+            // Default
+        }
+
+        public Motorway(string name)
+        {
+            Name = name;
+        }
+
+        public Motorway(string name, bool toll)
+        {
+            Name = name;
+            Toll = toll;
+        }
+    }
+}
---
+++ MotorwayClass/MotorwayClass/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MotorwayClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
+
+//Create a Motorway class that can be used as extra documentation with directions.Include data members such as name of motorway, type (i.e., Road, Street, Avenue, Blvd., Lane, etc.), direction (i.e., E, W, N, or S), surface (i.e., blacktop, gravel, sand, concrete), number of lanes, toll or no toll, and the party that maintains it.
+//Write instance methods that returns the full name of the motorway, full name of the motorway and whether it is toll or not, and full name of the motorway and the number of lanes.
+//Also include a ToString( ) method that returns all data members with appropriate labels.
+//Include enough constructors to make the class flexible.
